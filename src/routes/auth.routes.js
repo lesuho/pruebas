4c65652244414b7b21b93a14f2081b8dfe68001b
lesuho <75497467@continental.edu.pe>
2025-04-09 15:18:15 -0500

    Add files via upload

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import {Router} from 'express';
-import { login, register } from '../controllers/auth.controller.js' 
+import { login, logout, register } from '../controllers/auth.controller.js' 
 
 const router = Router()
 
 router.post('/register', register);
 
-router.post('/login', login)
+router.post('/login', login);
 
-export default router
+router.post('/logout', logout);
+
+export default router;
